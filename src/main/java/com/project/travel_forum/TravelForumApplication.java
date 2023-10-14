24819168c3dd5e70eba7d3e41e7fe2e3502e276b
simonanedeva <simona.nedeva2@gmail.com>
2023-10-14 14:09:22 +0300

    SpringBoot

---
+++ src/main/java/com/project/travel_forum/TravelForumApplication.java
@@ -0,0 +1,13 @@
+package com.project.travel_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/travel_forum/TravelForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.travel_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
